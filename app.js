7271f583a6699244f89ee2f366c15b980a90a66d
ahmad-zen <58863150+ahmad-zen@users.noreply.github.com>
2021-04-07 10:22:27 -0700

    init sim in app.js

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const http = require('http');
+const url = require('url');
+//const uuidv4 = require('uuid/v4');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
 
+/*
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +25,111 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+*/
+
+//--------MOVE OUT AND REQUIRE---------
+//
+var dayInMinutes = 10;
+function getSinWave(t) {
+    const amplitude = 500;
+    const shiftx = 0.5 * Math.PI;
+    const frequency = 2 * Math.PI / dayInMinutes;
+    return amplitude * Math.sin(frequency * t - shiftx) + amplitude;
+}
+class EnergyReading {
+    constructor(value) {
+        this.Value = value;
+        this.Unit = "WH";
+    }
+}
+class Device {
+    constructor() {
+        this.Id = 'newId'; //uuidv4();
+        this.DAY_ENERGY = new EnergyReading(0);
+        this.TOTAL_ENERGY = new EnergyReading(0);
+    }
+    generateEnergy(energyGenerated) {
+        this.DAY_ENERGY.Value += energyGenerated;
+        this.TOTAL_ENERGY.Value += energyGenerated;
+    }
+    resetDayEnergy() {
+        this.DAY_ENERGY.Value = 0;
+    }
+}
+class DeviceSimulator {
+    constructor() {
+        this.devices = new Array();
+    }
+    getDevices() {
+        return this.devices;
+    }
+    provisionDevice() {
+        const device = new Device();
+        this.devices.push(device);
+        return device.Id;
+    }
+    generateEnergy(energyGenerated) {
+        for (let i = 0; i < this.devices.length; i++) {
+            this.devices[i].generateEnergy(energyGenerated);
+        }
+    }
+    resetDevices() {
+        for (let i = 0; i < this.devices.length; i++) {
+            this.devices[i].resetDayEnergy();
+        }
+    }
+}
+class InverterSimulator {
+    constructor() {
+        this.minuteInMilliseconds = 1000 * 60;
+        this.minuteCounter = 0;
+        this.deviceSimulator = new DeviceSimulator();
+        this.deviceSimulator.provisionDevice();
+    }
+    provisionDevice() {
+        return this.deviceSimulator.provisionDevice();
+    }
+    getResponse() {
+        const response = this.deviceSimulator.getDevices();
+        this.deviceSimulator.resetDevices();
+        return response;
+    }
+    generateEnergyEveryMinute() {
+        const self = this;
+        setInterval(() => {
+            //todo: add some randomness to the generateEnergy
+            const energyGenerated = getSinWave(self.minuteCounter);
+            self.deviceSimulator.generateEnergy(energyGenerated);
+            if (self.minuteCounter < 10) {
+                self.minuteCounter += 1;
+            }
+            else {
+                self.minuteCounter = 0;
+            }
+        }, self.minuteInMilliseconds);
+    }
+}
+//
+//--------MOVE OUT AND REQUIRE---------
+
+//setup routes
+// GET devices
+app.get('/solar_api/v1/GetInverterRealtimeData.cgi', function (req, res) {
+	  res.writeHead(200, {
+		'Content-Type': "application/json"
+	});
+	const response = inverterSimulator.getResponse();
+	res.end(JSON.stringify(response));
+})
+
+// POST provision device
+app.post('/provision', function (req, res) {
+	  res.writeHead(200, {
+		'Content-Type': "application/json"
+	});
+	const response = inverterSimulator.provisionDevice();
+	res.end(response);
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
