6301119b0e0772affc6a38cafe3e81f7863e4943
ahmad-zen <58863150+ahmad-zen@users.noreply.github.com>
2021-04-07 14:51:30 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const http = require('http');
-const url = require('url');
-//const uuidv4 = require('uuid/v4');
+const uuidv4 = require('uuid.v4');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-var app = express();
-
-/*
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-*/
-
-//--------MOVE OUT AND REQUIRE---------
-//
 var dayInMinutes = 10;
 function getSinWave(t) {
     const amplitude = 500;
@@ -44,7 +15,7 @@ class EnergyReading {
 }
 class Device {
     constructor() {
-        this.Id = 'newId'; //uuidv4();
+        this.Id = uuidv4();
         this.DAY_ENERGY = new EnergyReading(0);
         this.TOTAL_ENERGY = new EnergyReading(0);
     }
@@ -56,17 +27,19 @@ class Device {
         this.DAY_ENERGY.Value = 0;
     }
 }
-class DeviceSimulator {
+class InverterCloudSimulator {
     constructor() {
+        this.minuteInMilliseconds = 1000 * 60;
+        this.minuteCounter = 0;
         this.devices = new Array();
-    }
-    getDevices() {
-        return this.devices;
     }
     provisionDevice() {
         const device = new Device();
         this.devices.push(device);
         return device.Id;
+    }
+    getDevices() {
+        return this.devices;
     }
     generateEnergy(energyGenerated) {
         for (let i = 0; i < this.devices.length; i++) {
@@ -78,20 +51,9 @@ class DeviceSimulator {
             this.devices[i].resetDayEnergy();
         }
     }
-}
-class InverterSimulator {
-    constructor() {
-        this.minuteInMilliseconds = 1000 * 60;
-        this.minuteCounter = 0;
-        this.deviceSimulator = new DeviceSimulator();
-        this.deviceSimulator.provisionDevice();
-    }
-    provisionDevice() {
-        return this.deviceSimulator.provisionDevice();
-    }
     getResponse() {
-        const response = this.deviceSimulator.getDevices();
-        this.deviceSimulator.resetDevices();
+        const response = this.getDevices();
+        this.resetDevices();
         return response;
     }
     generateEnergyEveryMinute() {
@@ -99,7 +61,7 @@ class InverterSimulator {
         setInterval(() => {
             //todo: add some randomness to the generateEnergy
             const energyGenerated = getSinWave(self.minuteCounter);
-            self.deviceSimulator.generateEnergy(energyGenerated);
+            self.generateEnergy(energyGenerated);
             if (self.minuteCounter < 10) {
                 self.minuteCounter += 1;
             }
@@ -109,42 +71,6 @@ class InverterSimulator {
         }, self.minuteInMilliseconds);
     }
 }
-//
-//--------MOVE OUT AND REQUIRE---------
+const inverterCloudSimulator = new InverterCloudSimulator();
 
-//setup routes
-// GET devices
-app.get('/solar_api/v1/GetInverterRealtimeData.cgi', function (req, res) {
-	  res.writeHead(200, {
-		'Content-Type': "application/json"
-	});
-	const response = inverterSimulator.getResponse();
-	res.end(JSON.stringify(response));
-})
-
-// POST provision device
-app.post('/provision', function (req, res) {
-	  res.writeHead(200, {
-		'Content-Type': "application/json"
-	});
-	const response = inverterSimulator.provisionDevice();
-	res.end(response);
-})
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+exports.inverterCloudSimulator = inverterCloudSimulator;
